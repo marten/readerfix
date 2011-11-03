5eb753f18e74d735036c830a7ac3a59e5b4ea77f
Marten Veldthuis <marten@veldthuis.com>
2011-11-03 11:25:15 +0100

    add failing test for reader endpoint

--- spec/readerfix_spec.rb
+++ spec/readerfix_spec.rb
@@ -1,7 +1,17 @@
+require 'rack/test'
+
 require 'readerfix'
 
 describe ReaderFix do
-  it 'should fail' do
-    fail "implement me"
+  include Rack::Test::Methods
+
+  def app
+    ReaderFix
+  end
+
+  it "should respond to google reader endpoint" do
+    get '/share?source=mysource&title=mytitle&url=myurl&short-url=myshort-url'
+    last_response.should be_ok
+    fail "Write more tests"
   end
 end
