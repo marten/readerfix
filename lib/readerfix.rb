d3f312ec9911ac7f89022544ed6fc85b88e9b484
Marten Veldthuis <marten@veldthuis.com>
2011-11-03 21:26:21 +0100

    add +1 button and warning

--- lib/readerfix.rb
+++ lib/readerfix.rb
@@ -20,6 +20,8 @@ class ReaderFix < Sinatra::Base
 
       Hallo. Welkom op onze mooie site.
 
+      Google Reader haalt sharing weg, wij geven het terug!
+
       Om dit te gebruiken, moet je in Google Reader een "Send To" ding maken.  De URL die google reader wil is:
 
       http://#{request.host}/MIJNUSERNAME/VETGEHEIMTOKEN/share?source=${source}&title=${title}&url=${url}&shorturl=${short-url}
@@ -31,6 +33,12 @@ class ReaderFix < Sinatra::Base
       Vervolgens klik je bij toffe dingen in reader op Send To -> ReaderFix.
 
 
+      !!! Deze dienst is nog experimenteel, noem het een publieke alpha of zo. Dit betekent dat shit
+      !!! gewoon kan verdwijnen. Als het wat wordt verplaatsen we 'm bovendien misschien wel.
+      !!! Dan moet je je configuratie van Reader misschien aanpassen (maar dan gooien we
+      !!! wel een berichtje in je feed).
+
+
                                               Groetjes,
 
                                                 Mark & Marten
@@ -40,6 +48,18 @@ class ReaderFix < Sinatra::Base
 
       PS. Het token VETGEHEIMTOKEN is dat niet meer. Misschien wil je een andere.
       </pre>
+
+      <!-- Place this tag where you want the +1 button to render -->
+      <g:plusone annotation="inline"></g:plusone>
+
+      <!-- Place this render call where appropriate -->
+      <script type="text/javascript">
+        (function() {
+          var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+          po.src = 'https://apis.google.com/js/plusone.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+        })();
+      </script>
     END
   end
 
